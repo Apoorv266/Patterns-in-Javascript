7b64384647b9562ca231951184b1965895941bcb
Apoorv <apoorvsharma494@gmail.com>
2022-05-18 23:38:42 +0530

    commit

--- Star/Pattern_7.js
+++ Star/Pattern_7.js
@@ -3,62 +3,56 @@
 //   *   *    
 //  *     *  
 // *       *
- 
-
-// for(let r = 1; r <= 5; r++){
-//     for (j = 5; j > r; j--)
-//     document.write("&nbsp;&nbsp;");
-//     for(let c = 1; c < r*2 ; c++){
-//     // if(c === 3){
-//     // }
-//     document.write("*")
-// }
-//     document.write("<br>")
-// }
 
 
-// var i, j, k;
-// for(i=1; i<=5; i++)
-// {
-//  for(j=5; j>=1; j--)
-//  console.log("let j :" ,j ,"let i :",i)
-//  {
-//   if(i == j)
-//    document.write("*");
-  
-//   else
-//    document.write("&nbsp;&nbsp;");
-//  } 
-//  for(k=2; k<=5; k++)
-//  {
-//   if(i == k)
-//    document.write("*");
-//   else
-//    document.write("&nbsp;&nbsp;");
-//  }
-//  document.write("<br>");
-//  console.log("------------")
-// }
-
-for(let i = 5; i >= 1; i--){
-for(let r = 1; r <= i; r++){
-    if (r === i){
-        document.write("*")
-    }
-
-    else{
-        document.write("&")
-    }
+for(let r = 1; r <= 5; r++){
+    for (j = 5; j > r; j--)
+    document.write("&nbsp;&nbsp;");
+    for(let c = 1; c < r*2 ; c++){
+    // if(c === 3){
+    // }
+    document.write("*")
+}
+    document.write("<br>")
 }
 
-// for (let k = 2; k <= i ; k++){
-//     if(k === i ){
-//         document.write("*")
-//     }
-//     else{
-//         document.write("&")
-//     }
-// }
 
-document.write("<br>")
+
+//..................................................................................
+
+
+for (let i = 1; i <= 5; i++) {
+    for (let r = i; r <= 5; r++) {
+        if (r === 5) {
+            document.write("*")
+        }                                                     
+        else {
+            document.write("&nbsp;&nbsp;")
+        }
+    }
+
+    for (let k = 2; k <= i; k++) {
+        document.write("&nbsp;&nbsp;")
+    }
+
+    for (let n = 2; n <= 5; n++) {
+        if (n === i) {
+            document.write("*")
+        }
+
+        else {
+            document.write("&nbsp;&nbsp;")
+        }
+    }
+    document.write("<br>")
 }
+
+
+
+
+
+// WE DID THIS QUESTION IN THREE PARTS BY DIVIDING IT INTO 3 TRIANGLES 
+
+
+
+

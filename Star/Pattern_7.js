212fd6637689b947d0d630f5afdc83a08c30fdc8
Apoorv <apoorvsharma494@gmail.com>
2022-05-18 23:47:24 +0530

    commit

--- Star/Pattern_7.js
+++ Star/Pattern_7.js
@@ -6,9 +6,9 @@
 
 
 for(let r = 1; r <= 5; r++){
-    for (j = 5; j > r; j--)
+    for (j = 5; j > r; j--)                       // first decending space pattern
     document.write("&nbsp;&nbsp;");
-    for(let c = 1; c < r*2 ; c++){
+    for(let c = 1; c < r*2 ; c++){                // then ascending star pattern
     document.write("*")
 }
     document.write("<br>")

c428206cd9641be098eaff394348bcde9d88129a
Apoorv <apoorvsharma494@gmail.com>
2022-05-21 22:26:24 +0530

    commit

--- Star/Pattern_9.js
+++ Star/Pattern_9.js
@@ -32,5 +32,35 @@ for (let i = 1; i <= 5; i++) {
             document.write("&nbsp;&nbsp;")
         }
     }
+
+
+    document.write("<br>")
+}
+for (let r = 1; r <= 5; r++) {
+
+    for (let c = 1; c <= r; c++) {
+        if (c === r) {
+            document.write("*")
+        }
+
+        else {
+            document.write("&nbsp;&nbsp;")
+        }
+    }
+
+    for(let k = r; k < 5; k ++){
+        document.write("&nbsp;&nbsp;")
+    }
+
+    for (let j = r ; j < 5; j++){
+          
+        if(j === 4){
+            document.write("*")  
+        }
+        else{
+            document.write("&nbsp;&nbsp;")
+        }
+    }
+
     document.write("<br>")
 }

0de23ec41ae65e9fb7611ef1d7130ae4ea4f4913
Apoorv <apoorvsharma494@gmail.com>
2022-05-22 00:11:33 +0530

    commit

---
+++ Star/Pattern_11.js
@@ -0,0 +1,32 @@
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+
+
+for (let i = 1; i <= 5 ; i++){
+
+    for(let j = i; j <= 5; j++){
+        document.write("*")
+    }
+
+    for (let s = 1; s < i  ; s++ ){
+        document.write("&nbsp;&nbsp;")
+    }
+
+    for (let s = 1; s < i  ; s++ ){
+        document.write("&nbsp;&nbsp;")
+    }
+
+    for(let j = i; j <= 5; j++){
+        document.write("*")
+    }
+    document.write("<br>")
+}
+
+
